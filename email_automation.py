0a1188e0235c74b530eff6b7e8fc022525b84edf
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2023-04-10 02:25:13 +0530

    Update email_automation.py

--- email_automation.py
+++ email_automation.py
@@ -12,7 +12,7 @@ from openpyxl import load_workbook
 import pandas as pd
 
 #Load the user name and passwd 
-user, password = "udaysankar2003@gmail.com","yxoqclmsdmqsjxeg"
+user, password = "udaysankar2003@gmail.com",""
 
 #URL for IMAP connection
 imap_url = 'imap.gmail.com'
